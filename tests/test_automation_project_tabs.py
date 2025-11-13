d2f9bc62e50fd4e85115e65bd829cf68d5e05811
Prasad <prasad@LAPTOP-QE5Q58AD>
2025-11-13 11:09:08 +0530

    Fix typo in log message and adjust sleep timing in test for better clarity

--- tests/test_automation_project_tabs.py
+++ tests/test_automation_project_tabs.py
@@ -19,7 +19,7 @@ class TestNavigateProjectTabs():
 
         brand_page_nevigations.click_all_title_logo() # clicking the all title logo
         brand_page_nevigations.click_test_automation_project() # clicking the test automation project
-
+        time.sleep(2)  # time sleep to see the default landing section 
         brand_page_nevigations.click_details_section() # click the details tab to visit the details section
         time.sleep(10) # time sleep to keep the video section on the view port
         brand_page_nevigations.click_video_section() # click the details tab to visit the video section
